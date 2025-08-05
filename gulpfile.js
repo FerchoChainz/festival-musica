f5dc774b7f65887a7e2733bc2c3cd4303ef28ab4
FerchoChainz <tragicomedia9@gmail.com>
2025-08-05 01:31:09 -0600

    Added semantic HTML just to improve SCSS skills. And more mixins and layouts

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,19 @@
-import { src, dest, watch } from 'gulp'
-import * as dartSass from 'sass'
-import gulpSass from 'gulp-sass'
+import { src, dest, watch } from "gulp";
+import * as dartSass from "sass";
+import gulpSass from "gulp-sass";
 
-const sass = gulpSass(dartSass)
+const sass = gulpSass(dartSass);
 
+export function css(done) {
+  src("src/scss/app.scss", {sourcemaps: true}) //ubica el archivo
+    .pipe(sass().on("error", sass.logError)) //para escuchar errores
+    .pipe(dest("dist/css", {sourcemaps: true})); //donde lo guardara
 
-export function css (done) {
-    src('src/scss/app.scss') //ubica el archivo
-    .pipe(sass().on('error', sass.logError)) //para escuchar errores
-    .pipe(dest('dist/css')) //donde lo guardara
-
-    done()
+  done();
 }
 
-export function dev(){
-    watch('src/scss/**/*.scss', css)
-    //Aqui indicamos que archivos vamos a estar observando
-    //pendiente de cambios y ejecutamos la funcion css
-
+export function dev() {
+  watch("src/scss/**/*.scss", css);
+  //Aqui indicamos que archivos vamos a estar observando
+  //pendiente de cambios y ejecutamos la funcion css
 }
